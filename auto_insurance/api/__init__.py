da0849d40f723e5e5cd0b2b174430549e8936daf
anasthasia06 <daunesanas@cy-tech.fr>
2026-03-26 13:23:29 +0100

    config: setup __init__ files

---
+++ auto_insurance/api/__init__.py
@@ -0,0 +1,1 @@
+# P2 remplira ce fichier avec les endpoints FastAPI
---
+++ auto_insurance/src/__init__.py
@@ -0,0 +1,3 @@
+from .preprocessing import DataPreprocessor
+from .features import FeatureEngineer
+from .model import InsuranceModel
